c37863d0eb3c652e57ff1bc924f4f54753f2358c
iamgauravpande <gaupande21@gmail.com>
2025-05-21 09:14:14 +0530

    re-deploy argocd

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-# resource "helm_release" "argo_cd" {
-#   name             = "argocd"
-#   repository       = "https://argoproj.github.io/argo-helm"
-#   chart            = "argo-cd"
-#   namespace        = "argocd"
-#   create_namespace = true
-#   version          = "8.0.6"
-#   values = [
-#     file("${path.root}/helm-values/argocd.yaml")
-#   ]
-# }
+resource "helm_release" "argo_cd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "8.0.6"
+  values = [
+    file("${path.root}/helm-values/argocd.yaml")
+  ]
+}
