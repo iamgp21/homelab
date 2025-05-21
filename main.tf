00f14aa3b0597915e10ff350a9ea7b431a4df42c
iamgauravpande <gaupande21@gmail.com>
2025-05-21 09:31:23 +0530

    added force_update

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ resource "helm_release" "argo_cd" {
   namespace        = "argocd"
   create_namespace = true
   version          = "8.0.6"
-  upgrade_install = true
-  replace = true  
+  replace = true
+  force_update = true
   values = [
     file("${path.root}/helm-values/argocd.yaml")
   ]
